fbe69abcb37523278862c34cf5b5a4817c493ebb
emersonbraga <114742361+bragaemerson@users.noreply.github.com>
2022-10-21 14:52:25 +0000

    recebendo listas de despesas. #12

---
+++ listadedespesas.js
@@ -0,0 +1,15 @@
+function listarDespesas() {
+  console.log("insira despesas com energia");
+  energia = parseFloat(prompt(""));
+  12
+
+  console.log("insira despesas com agua");
+  agua = parseFloat(prompt(""));
+
+  console.log("insira despesas com gas");
+  gas = parseFloat(prompt(""));
+
+  console.log("insira despesas com material de limpeza");
+  materialdelimpeza = parseFloat(prompt(""));
+  return [energia,agua,gas,materialdelimpeza]
+}
