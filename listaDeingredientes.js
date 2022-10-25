e02a30e0cd6244a1ff5efeaf04bc295d3482da3f
Marco Aurelio Smak Affonso <114742012+marcoskaff02@users.noreply.github.com>
2022-10-25 09:25:50 -0300

    lista de ingredientes

---
+++ listaDeingredientes.js
@@ -0,0 +1,8 @@
+let Ningredientes = parseInt(prompt("Informe o número de ingredientes"));
+let ingredientes = [];
+console.log("Adicione os ingredientes");
+for (let i = 1; i <= Ningredientes; i++) {
+ingredientes.push(prompt(""));
+}
+
+console.log(ingredientes);
