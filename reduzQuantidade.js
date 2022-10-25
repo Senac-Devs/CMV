bac35abec1b5322f403358feb59330560097a599
Alexsandro <114742559+alexsandrocm@users.noreply.github.com>
2022-10-25 13:42:43 +0000

    Função reduz quantidade

---
+++ reduzQuantidade.js
@@ -0,0 +1,6 @@
+function reduzQuantidadePeso(quantidade, unidade) {
+  if (unidade.toUpperCase() === "G") {
+    return quantidade
+  } else if (unidade.toUpperCase() === "KG")
+    return (quantidade * 1000)
+};
