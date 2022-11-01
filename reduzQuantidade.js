7a96d3e77bb61beec16375519495636b60e45448
AlissonReifur <alisssonreifur@gmail.com>
2022-11-01 11:28:34 -0300

    atualizaçao do reduzQuantidadePeso

--- reduzQuantidade.js
+++ reduzQuantidade.js
@@ -1,6 +1,16 @@
 function reduzQuantidadePeso(quantidade, unidade) {
-    if (unidade.toUpperCase() === "G") {
-        return quantidade
-    } else if (unidade.toUpperCase() === "KG")
-        return (quantidade * 1000)
-};
+    console.log(quantidade, unidade);
+    if (unidade.toUpperCase() === "MG") {
+        return quantidade / 1000;
+    } else if (unidade.toUpperCase() === "G") {
+        return quantidade;
+    } else if (unidade.toUpperCase() === "KG") {
+        return quantidade * 1000;
+    } else if (unidade.toUpperCase() === "L") {
+        return quantidade * 1000;
+    } else if (unidade.toUpperCase() === "ML") {
+        return quantidade;
+    } else if (unidade.toUpperCase() === "UND") {
+        return quantidade;
+    }
+}
